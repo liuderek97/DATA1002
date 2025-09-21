33c39e0c7f1f48a605d07038a56e42666fe5a37a
Derek Liu <liuderek97@gmail.com>
2025-09-21 11:36:03 +1000

    Filter applied to Premesis CSV
    Filter for business that are currently trading, are full hotels(pubs/clubs), in the suburb of Sydney, and trade late.

---
+++ filter.py
@@ -0,0 +1,20 @@
+import pandas as pd
+df = pd.read_csv("premises-list-as-at-7-jun-2024.csv")
+
+trading_status = "Trading" 
+lga =  "Sydney"
+postcode = 2000
+license_type = ["Liquor - on-premises licence", "Liqour - small bar licence", "Liquor - hotel licence"]
+license_status = "Current"
+business_type = "Full hotel"
+precinct = "Sydney CBD Precinct"
+street_names = ["Bridge", "Elizabeth", "Park", "Kent"]
+#invalid_business_type = ["Restaurant", "Bottle shops & delivery", "Accomodation", "Catering service,Restaurant", "Catering service", "Vessel",
+#                        "Theatre public entertainment venue", "Cooking school", "Co-shared Workspace", "Art gallery", "Delivery Only", "Catering service,Theatre public entertainment venue", "Bar,Catering service,Public arena and events,Public hall,Theatre public entertainment venue"]
+
+df_filter = df[(df['LGA'] == lga) & (df['Licence type'].isin(license_type)) & (df["Trading Status"] == trading_status) 
+& (df["Status"] == license_status) & (df['Business type'] == business_type) & (df["To 12am"] == "Yes") & (df["To 3am"] == "Yes") & df["Suburb"] == "Sydney"]
+
+
+
+df_filter.to_csv("filtered_premises_list.csv")
